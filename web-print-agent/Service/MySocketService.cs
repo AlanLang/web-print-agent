a37fceb41a0dad00a3854731fe2af792f5ffe5ec
Alan <langwdalan@gmail.com>
2018-09-17 10:19:59 +0800

    :sparkles: 加入socket服务

---
+++ web-print-agent/Service/MySocketService.cs
@@ -0,0 +1,19 @@
+﻿using Sodao.FastSocket.Server;
+using Sodao.FastSocket.Server.Command;
+using Sodao.FastSocket.SocketBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_print_agent.Service
+{
+    public class MySocketService : CommandSocketService<AsyncBinaryCommandInfo>
+    {
+        protected override void HandleUnKnowCommand(IConnection connection, AsyncBinaryCommandInfo commandInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
