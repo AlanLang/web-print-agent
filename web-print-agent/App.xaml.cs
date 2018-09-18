a85bbddfa0d9fa9459c778bdf1cd3acef0f4aaf2
Alan <langwdalan@gmail.com>
2018-09-18 14:10:53 +0800

    :sparkles: 加入全局异常捕捉

--- web-print-agent/App.xaml.cs
+++ web-print-agent/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using web_print_agent.Utils;
 
 namespace web_print_agent
 {
@@ -16,12 +17,39 @@ namespace web_print_agent
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            int i = e.Args.Length;
             log4net.Config.XmlConfigurator.Configure();
+
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             base.OnStartup(e);
         }
         protected override void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
+        }
+
+        //主线程异常
+        void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+                Service.MyLogService.Error("未捕获的主线程异常" + e.Exception.Message, e.Exception);
+                MessageBox.Show(e.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("应用程序发生不可恢复的异常，将要退出！");
+            }
+
+        }
+
+        //非主线程异常
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Service.MyLogService.Error("未捕获的子线程异常", e.ExceptionObject as Exception);
         }
     }
 }
