10ace430dd545dd27d1a343aac93edf5780479ae
Alan <langwdalan@gmail.com>
2018-09-19 16:13:56 +0800

    :sparkles: 加入文字打印和线条打印功能

--- web-print-agent/Service/PrintService.cs
+++ web-print-agent/Service/PrintService.cs
@@ -16,6 +16,7 @@ namespace web_print_agent.Service
         protected string err;
         protected PrintDocument pd;
         protected string fontFamily = "正常";
+        protected int Pixel;
         public string Err { get { return err; } }
 
         public bool SetPrintOrder(string printOrder)
@@ -39,6 +40,7 @@ namespace web_print_agent.Service
             Double id = printOrder.id;
             pd = new PrintDocument();
             this.setPageSize();
+            Pixel = pd.DefaultPageSettings.PrinterResolution.X;// 获取分辨率
             pd.PrintPage += pd_PrintPage;
             pd.PrintController = new StandardPrintController();             //去掉打印弹出框
             pd.Print();
@@ -47,14 +49,48 @@ namespace web_print_agent.Service
         void pd_PrintPage(object sender, PrintPageEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.DrawString("物料转运标识单", new Font(fontFamily, 20), System.Drawing.Brushes.Black, 300, 67);
+            var content = printOrder.content;
+            foreach (var item in content)
+            {
+                string type = item.type;
+                switch (type)
+                {
+                    case "text":
+                        printText(g,item);
+                        break;
+                    case "line":
+                        printLine(g, item);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void printLine(Graphics g, dynamic item)
+        {
+            float x1 = toInches(item.x1);
+            float y1 = toInches(item.y1);
+            float x2 = toInches(item.x2);
+            float y2 = toInches(item.y2);
+            Pen pen = new Pen(Color.Black, 1);
+            g.DrawLine(pen, x1,y1,x2,y2);
+        }
+
+        private void printText(Graphics g, dynamic item)
+        {
+            string text = item.text;
+            int size = toInt(item.size);
+            float x = toInches(item.x);
+            float y = toInches(item.y);
+            g.DrawString(text, new Font(fontFamily, size), System.Drawing.Brushes.Black, x, y);
         }
 
         protected void setPageSize()
         {
             string pageName = printOrder.id.ToString();
-            int width = toInches(printOrder.page.width);
-            int height = toInches(printOrder.page.height);
+            int width = tomInches(printOrder.page.width);
+            int height = tomInches(printOrder.page.height);
             PaperSize ps = new PaperSize("Custum", width, height);
             pd.DocumentName = pageName;
             pd.DefaultPageSettings.PaperSize = ps;
@@ -62,9 +98,19 @@ namespace web_print_agent.Service
             pd.DefaultPageSettings.PaperSize.RawKind = 256;
         }
 
-        public int toInches(double mm)
+        public int tomInches(double mm)
         {
             return (int)Math.Round(mm*3.937);
+        }
+
+        public float toInches(double num)
+        {
+            return Convert.ToSingle(num * 3.937008);
+        }
+
+        public int toInt(dynamic i)
+        {
+            return Convert.ToInt32(i);
         }
     }
 }
